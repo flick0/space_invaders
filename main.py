978a0e2faf3805ee6e2cb9d494731bec1df6da96
flicko0 <flickerdroid211@gmail.com>
2022-05-22 16:29:41 +0530

    added bot

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 import os
 
 from bot import bot
 
 bot.run(os.environ["TOKEN"])
-=======
-from os import environ
-from discord.ext import commands
-
-class SpaceInvaders(commands.Bot):
-    def run(self):
-        super().run(environ['TOKEN'])
->>>>>>> 6125b0a137b11fa58415e007a3c77b934ee47fc1
