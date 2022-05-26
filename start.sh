f5f3f56e05471b64afef28091d0ff5ea0278d9f3
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-05-26 07:36:21 +0100

    Extra modifications

--- start.sh
+++ start.sh
@@ -7,13 +7,15 @@ else
     echo "Using pre-existing .env file"
 fi;
 
-echo "Building Dockerfile for bot"
+echo "Creating volume for MongoDB"
 
 docker volume create mongodb_volume
+
+echo "Building Dockerfile for bot"
 
 docker build -t tcr_event_bot .
 
 echo "Finished building Dockerfile"
 
 echo "Running RocketRider!"
-docker-compose up -d --env-file=.env
+docker-compose up -d
